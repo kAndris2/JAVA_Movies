749dc66ce8ed56d921109a81905d88569ca10455
burgonyapure <deejay.spory@gmail.com>
2020-09-19 11:56:45 +0200

    Movie Details

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import Navbar from "./components/Navbar";
+import MovieDetail from "./components/MovieDetail";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+import Main from "./components/Main";
 
 class App extends Component {
   state = {
@@ -15,15 +23,13 @@ class App extends Component {
   async componentDidMount() {
     const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=04a30d5152c77afe4a81783d17e20316&language=hu-HU&page=1&region=HU');
     const movie = await response.json();
-
-    /*const resp = await fetch('/api/users');
-    const user = await  resp.json();*/
-
-    /*let movies = [...this.state.movies];
-    movies.push(movie);
-
-    this.setState({ movies });*/
     this.setState({movies: movie.results, isLoading: false });
+  }
+
+  getId(){
+    let path = window.location.href;
+    let id = String(path).split("/")[4];
+    return id;
   }
 
   render() {
@@ -37,46 +43,24 @@ class App extends Component {
     return (
         <div className="App">
           <Navbar></Navbar>
-          <div id="cards_landscape_wrap-2">
-            <div className="container" style={{maxWidth:"1300px", paddingLeft: "40px", paddingRight: "40px", paddingTop: "30px", paddingBottom: "30px"}}>
-              <div className="row">
-                {movies.map(movie =>
-                    <div key={movie.id} className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                      <a href={"/movie/"+movie.id} data-toggle="modal" data-target="#exampleModalCenter">
-                        <div className="card-flyer">
-                          <div className="text-box">
-                            <div className="image-box">
-                              <img style={{position: "relative"}} src={image_pre + movie.poster_path} alt="" />
-                            </div>
-                            <div className="text-container">
-                              <h6>{movie.title}</h6>
-                              <p>{movie.release_date}</p>
-                              <div style={{ width: "15%" , position:"absolute", top: "445px"}}>
-                                <CircularProgressbar
-                                  value={movie.vote_average}
-                                  maxValue={10}
-                                  text={`${movie.vote_average * 10}%`}
-                                  background={true}
-                                  styles={buildStyles({
-                                    strokeLinecap: 'round',
-                                    textSize: '2em',
-                                    pathTransitionDuration: 0.5,
-                                    pathColor: '#23c171',
-                                    textColor: '#ffffff',
-                                    trailColor: '#204629',
-                                    backgroundColor: '#0A1D34',
-                                  })}
-                                />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                )}
-              </div>
-            </div>
-          </div>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <Main
+                    movies={movies}
+                    image_pre={image_pre}>
+                </Main>
+              </Route>
+
+              <Route exact path={"/movie/"+this.getId()}>
+                <MovieDetail
+                    movie_id={this.getId()}>
+                </MovieDetail>
+              </Route>
+
+            </Switch>
+          </Router>
+
         </div>
     );
   }
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { faBell, faPlus, faUser, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Nav.css';
+import MovieDetail from "./MovieDetail";
 
 class Navbar extends Component {
     render() {
@@ -9,7 +10,7 @@ class Navbar extends Component {
             <React.Fragment>
                 <nav className="navbar navbar-expand-sm navbar-dark" style={{backgroundColor:"#032541", fontWeight:"bold", padding:".75rem .75rem"}}>
                     <div className="container" style={{maxWidth:"1300px", paddingLeft:"40px", paddingRight:"40px"}}>
-                        <a className="navbar-brand" href="#"><img src={require("../static/img/logo.png")} width={154} height={20}/> </a>
+                        <a className="navbar-brand" style={{paddingTop:"0"}} href="/"><img src={require("../static/img/logo.png")} width={154} height={20}/> </a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false"
                                 aria-label="Toggle navigation">
@@ -17,16 +18,16 @@ class Navbar extends Component {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarsExample07">
                             <ul className="navbar-nav mr-auto">
-                                <li className="nav-item active">
-                                    <a className="nav-link" href="#">Movies</a>
+                                <li className="nav-item px-2 active">
+                                    <a className="nav-link" href="/movie/test">Movies</a>
                                 </li>
-                                <li className="nav-item active">
+                                <li className="nav-item px-2 active">
                                     <a className="nav-link" href="#">TV Shows</a>
                                 </li>
-                                <li className="nav-item active">
+                                <li className="nav-item px-2 active">
                                     <a className="nav-link" href="#">People</a>
                                 </li>
-                                <li className="nav-item active">
+                                <li className="nav-item px-2 active">
                                     <a className="nav-link" href="#">More</a>
                                 </li>
                             </ul>
