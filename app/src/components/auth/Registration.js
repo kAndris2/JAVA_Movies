2db4506f84bac5d83b662d4b15453e4dd059aad6
burgonyapure <deejay.spory@gmail.com>
2020-09-22 11:45:50 +0200

    reg and pull

--- app/src/components/auth/Registration.js
+++ app/src/components/auth/Registration.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
 class Registration extends Component {
     constructor(props) {
         super(props);
@@ -18,7 +18,25 @@ class Registration extends Component {
     }
 
     handleSubmit(event){
-        console.log("form submuitntteed");
+        //destruct tho
+        const {
+            email,
+            password,
+            password_confirm,
+            username
+        } = this.state;
+
+        axios.post("http://localhost:3000/api/add_user", {
+            username: username,
+            email: email,
+            password: password
+        },
+            {withCredentials: true}
+        ).then(response => {
+            console.log("reg result", response);
+        }).catch( error => {
+            console.log("reg error", error);
+        })
         event.preventDefault();
     }
 
