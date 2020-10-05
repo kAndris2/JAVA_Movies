f9f3d62a55c0ea10d6dde578c55c419bd9f8d05d
kAndris2 <k.andris258@gmail.com>
2020-10-05 10:50:21 +0200

    env var

--- src/main/java/com/springboot/movies/database/IDAO.java
+++ src/main/java/com/springboot/movies/database/IDAO.java
@@ -9,9 +9,9 @@ import java.util.List;
 public final class IDAO {
     private static IDAO singleton = null;
 
-    final public String URL = "jdbc:postgresql://localhost:5432/MoviesDB";
-    final public String USER = "postgres";
-    final public String PASSWORD = "admin";
+    final public String URL = System.getenv("DB_URL");
+    final public String USER = System.getenv("DB_USER");
+    final public String PASSWORD = System.getenv("DB_PASSWORD");
 
     final public String USER_TABLE = "users";
     final public String PICTURE_TABLE = "pictures";
