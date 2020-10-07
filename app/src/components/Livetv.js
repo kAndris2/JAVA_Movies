4f2104b48e5380d4f8f7eeabda50d39c16da915a
burgonyapure <deejay.spory@gmail.com>
2020-10-07 12:03:57 +0200

    livetv

--- app/src/components/Livetv.js
+++ app/src/components/Livetv.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 
 class Livetv extends Component {
     constructor(props) {
@@ -6,30 +7,64 @@ class Livetv extends Component {
 
         this.state={
             channels: [],
+            epg:[],
+            selectedEpg:[],
+
             isLoading: true,
             selectedChannel:'175187478216bb9042221e84add7502b'
         }
         this.changeChannel=this.changeChannel.bind(this);
+        this.getEPG=this.getEPG.bind(this);
     }
 
     async componentDidMount() {
+        //GET THE CHANNELS
         const response = await fetch("http://mradmin.hu:9981/api/channel/list",{
                 method: 'get',
                 headers: new Headers({
                     'Authorization': 'Basic '+btoa('test:123qweAsD'),
                 })}
-            );
+        );
 
         const list = await response.json();
+
+        //GET THE EPG
+        /*const response2 = await fetch("http://mradmin.hu:9981/api/epg/events/grid?limit=100",{
+            method: 'get',
+                headers: new Headers({
+                'Authorization': 'Basic '+btoa('test:123qweAsD'),
+            })}
+        );
+        const epg = await response2.json();*/
+
+
+        await this.getEPG();
         this.setState({channels:list.entries, isLoading:false});
+
     }
 
-    changeChannel(channelId){
-        this.setState({selectedChannel:channelId});
+    async changeChannel(channelId){
+        this.setState({selectedChannel:channelId}, () => {
+            this.getEPG();
+        });
+    }
+
+    async getEPG(){
+        let channel = this.state.selectedChannel;
+
+        const response = await fetch('http://mradmin.hu:9981/api/epg/events/grid?channel='+channel+'&sort=start&limit=4',{
+            method: 'get',
+            headers: new Headers({
+                'Authorization': 'Basic '+btoa('test:123qweAsD'),
+            })
+        });
+        const channelEpg = await response.json();
+        this.setState({selectedEpg: channelEpg.entries});
+
     }
 
     render() {
-        if (this.state.isLoading){
+        if (this.state.isLoading && (this.state.selectedEpg.length === 0)){
             return (<div>Loading...</div>)
         }
         return (
@@ -44,7 +79,7 @@ class Livetv extends Component {
                                         <ul className="settings panel with_counts scroller">
                                             {this.state.channels.map(channel =>
                                                 <li key={channel.key} className="">
-                                                    <a href={"/livetv/#"} onClick={() => this.changeChannel(channel.key)}
+                                                    <a href={"/livetv/#"} onClick={() => {this.changeChannel(channel.key); }}
                                                        className="search_tab active" title="Channels"
                                                        alt="Channels">{channel.val}</a>
                                                     {/*<span>136</span>*/}
@@ -59,10 +94,48 @@ class Livetv extends Component {
                                 <section className="panel">
                                     <div className="search_results movie">
                                         <div className="results flex">
-                                            <video key={"http://mradmin.hu:9981/stream/channel/"+this.state.selectedChannel+"?profile=webtv-h264-aac-matroska"} width="800px" autoPlay controls style={{visibility: "visible"}}>
+                                            <video key={"http://mradmin.hu:9981/stream/channel/"+this.state.selectedChannel+"?profile=webtv-h264-aac-matroska"} width="100%" autoPlay controls style={{visibility: "visible",borderRadius: "8px"}}>
                                                 <source id="ext-gen3699" src={"http://mradmin.hu:9981/stream/channel/"+this.state.selectedChannel+"?profile=webtv-h264-aac-matroska"}/>
                                             </video>
                                         </div>
+                                        {this.state.selectedEpg.map((epg,index) =>
+                                            <div className="card v4 tight" style={(index === 0) ? {backgroundColor:"rgb(30,213,169)"}:{backgroundColor:"white"}}>
+                                                <div className="wrapper">
+                                                    <div className="details">
+                                                        <div className="wrapper">
+                                                            <div className="container">
+                                                                <div className="row">
+                                                                    <div className="col-sm-10">
+                                                                        <div className="title">
+                                                                            <a>
+                                                                                <h2>{epg.title}</h2>
+                                                                            </a>
+                                                                            <span className="release_date">
+                                                                            {moment.unix(epg.start).format("HH:mm")} - {moment.unix(epg.stop).format("HH:mm")}
+                                                                        </span>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div className="col-sm-2">
+                                                                        <p style={{fontStyle:"italic"}}>
+                                                                            {index > 0 &&
+                                                                                "Next Up"
+                                                                            }
+                                                                            {index === 0 &&
+                                                                                "Currently Playing"
+                                                                            }
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="overview">
+                                                            <p>{epg.summary}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )}
+
                                     </div>
                                 </section>
                             </div>
